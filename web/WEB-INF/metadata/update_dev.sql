ca4d10551aa13f6fe7798be261e1f8445b43d661
samuelpinheiro <samuelpinheiro@gmail.com>
2010-10-11 15:36:07 -0300

    arquivos modificados pelo samuel que no conseguimos fazer merge na primeira vez

--- web/WEB-INF/metadata/update_dev.sql
+++ web/WEB-INF/metadata/update_dev.sql
@@ -1,9 +1,3 @@
 update parametrosdosistema set appversao = '1.1.31.20';--.go
 
-alter table comissaomembro add column dataEnt date;--.go
-
-insert into papel values (nextval('papel_sequence'), 'ROLE_REL_ACOMPANHAMENTO_EXPERIENCIA', 'Periodo de Acompanhamento de Experiência',
- '/avaliacao/periodoExperiencia/prepareRelatorioAcopanhamentoExperiencia.action', 2, 't', null, 486);--.go
-
-insert into papel values (nextval('papel_sequence'), 'ROLE_REL_ACOMP_RANKING_PERIODO_EXPERIENCIA', 'Ranking Performace Periodo de Experiência',
- '/avaliacao/periodoExperiencia/prepareRelatorioRankingPerformancePeriodoDeExperiencia.action', 3, 't', null, 486);--.go
+alter table comissaomembro add column dataEnt date;
