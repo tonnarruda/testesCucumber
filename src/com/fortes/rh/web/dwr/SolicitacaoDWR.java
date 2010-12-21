5095721d2eabc63a39ed65fcb122c4a053cf52bd
atualpabruno <atualpabruno@gmail.com>
2010-12-21 10:32:47 -0300

    Arquivo solicitacaoDWR

---
+++ src/com/fortes/rh/web/dwr/SolicitacaoDWR.java
@@ -0,0 +1,23 @@
+package com.fortes.rh.web.dwr;
+
+import java.util.Collection;
+import java.util.Map;
+
+import com.fortes.rh.business.captacao.SolicitacaoManager;
+import com.fortes.rh.model.captacao.Solicitacao;
+import com.fortes.rh.util.CollectionUtil;
+
+public class SolicitacaoDWR {
+
+	private SolicitacaoManager solicitacaoManager;
+
+	public Map getSolicitacoes(Long empresaId) 
+	{
+		Collection<Solicitacao> solicitacaos = solicitacaoManager.findSolicitacaoList(empresaId, false, true, false);
+		return new CollectionUtil<Solicitacao>().convertCollectionToMap(solicitacaos, "getId", "getDescricaoFormatada");
+	}
+
+	public void setSolicitacaoManager(SolicitacaoManager solicitacaoManager) {
+		this.solicitacaoManager = solicitacaoManager;
+	}
+}
