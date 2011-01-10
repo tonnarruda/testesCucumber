d86c29fd14b93209dc2bbb41a54c5c795ed1edda
marlussaraiva <marlussaraiva@grupofortes.com.br>
2011-01-10 10:35:13 -0300

    Primeiros testes com o Remprot

--- src/com/fortes/rh/util/Autenticador.java
+++ src/com/fortes/rh/util/Autenticador.java
@@ -18,18 +18,18 @@ public class Autenticador
 		da licenca.
 		*/
 		//TODO remprot
-//		RPClient c = getRemprot(url);
+		RPClient c = getRemprot(url);
 
-//		System.out.println("applicationId: " + c.getApplicationId());	// codigo de reset do produto (numero fixo para cada produto: AC=1, AG=16, Nettion=22 etc)
-//		System.out.println("installationId: " + c.getInstallationId());	// numero de serie recebido atraves de codigo de resposta (unico por cliente, sequencial independente de produto)
-//		System.out.println("resetCounter: " + c.getResetCounter());	    // quantas vezes esta licenca já recebeu resets (ajuda a detectar fraudes)
-//		System.out.println("customerId: " + c.getCustomerId());		    // CPF ou CNPJ do cliente
-//		System.out.println("customerName: " + c.getCustomerName());	    // denominacao social do cliente
-//		System.out.println("nextCrashDate: " + c.getNextCrashDate());	// data limite da execucao (o remprot nao implementa os bloqueios, o sistema deve faze-lo)
-//		System.out.println("errors: " + c.getErrors());			        // se errors==0 entao nao houve problemas na conversa com o servidor
-//		System.out.println("registered: " + c.getRegistered());		    // se nao esta registrado entao é cópia pirata (ou maquina nova, dá no mesmo)
+		System.out.println("applicationId: " + c.getApplicationId());	// codigo de reset do produto (numero fixo para cada produto: AC=1, AG=16, Nettion=22 etc)
+		System.out.println("installationId: " + c.getInstallationId());	// numero de serie recebido atraves de codigo de resposta (unico por cliente, sequencial independente de produto)
+		System.out.println("resetCounter: " + c.getResetCounter());	    // quantas vezes esta licenca já recebeu resets (ajuda a detectar fraudes)
+		System.out.println("customerId: " + c.getCustomerId());		    // CPF ou CNPJ do cliente
+		System.out.println("customerName: " + c.getCustomerName());	    // denominacao social do cliente
+		System.out.println("nextCrashDate: " + c.getNextCrashDate());	// data limite da execucao (o remprot nao implementa os bloqueios, o sistema deve faze-lo)
+		System.out.println("errors: " + c.getErrors());			        // se errors==0 entao nao houve problemas na conversa com o servidor
+		System.out.println("registered: " + c.getRegistered());		    // se nao esta registrado entao é cópia pirata (ou maquina nova, dá no mesmo)
 
-		return true;//c.getRegistered();
+		return c.getRegistered();
 	}
 
 	public static RPClient getRemprot(String url)
@@ -53,11 +53,10 @@ public class Autenticador
 	public static String getMsgAutenticado(String url)
 	{
 		//TODO remprot
-//		if(getRemprot(url).getRegistered())
-//			return "";
-//		else
-//			return getMsgPadrao();
-		return "";
+		if(getRemprot(url).getRegistered())
+			return "";
+		else
+			return getMsgPadrao();
 	}
 
 	public static int getQtdCadastrosVersaoDemo()
