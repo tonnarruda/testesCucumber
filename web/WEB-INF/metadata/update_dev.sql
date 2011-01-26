14cfe9bcfa004d50a3ecb4461529f46b109ccab2
atualpabruno <atualpabruno@gmail.com>
2011-01-26 17:17:49 -0300

    Sql do cartao de envio de email de exames previstos

--- web/WEB-INF/metadata/update_dev.sql
+++ web/WEB-INF/metadata/update_dev.sql
@@ -8,3 +8,9 @@ INSERT INTO papel (id, codigo, nome, url, ordem, menu, papelmae_id) VALUES (496,
 alter sequence papel_sequence restart with 497;--.go
 
 update candidato set disponivel = false where blacklist = true;--.go
+
+update papel set ordem = ordem + 1 where papelmae_id = 75--.go
+
+INSERT INTO papel (id, codigo, nome, url, ordem, menu, papelmae_id) VALUES (497, 'ROLE_RECEBE_EXAMES_PREVISTOS', 'Recebe email de exames previstos', '#', 1, false, 75);--.go
+
+alter sequence parametrosdosistema_sequence restart with 498; --.go
