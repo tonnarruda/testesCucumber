0bd715ac6e57e596e46a64e1ea88a2a8323c7987
Rafael Ponte <rponte@gmail.com>
2011-03-09 19:14:18 -0300

    Melhora cobertura dos testes;

--- test/com/fortes/rh/test/dicionario/TipoAvaliacaoTest.java
+++ test/com/fortes/rh/test/dicionario/TipoAvaliacaoTest.java
@@ -2,6 +2,7 @@ package com.fortes.rh.test.dicionario;
 
 import junit.framework.TestCase;
 
+import com.fortes.rh.model.dicionario.Apto;
 import com.fortes.rh.model.dicionario.TipoAvaliacao;
 
 public class TipoAvaliacaoTest extends TestCase
@@ -11,6 +12,13 @@ public class TipoAvaliacaoTest extends TestCase
 		TipoAvaliacao tipo = new TipoAvaliacao();
 	
 		assertEquals(3, tipo.size());
+	}
+	
+	public void testGetAvaliacoes() {
+		TipoAvaliacao tipo = new TipoAvaliacao();
+		assertEquals("Pesquisa", tipo.get(TipoAvaliacao.PESQUISA));
+		assertEquals("Avaliação", tipo.get(TipoAvaliacao.AVALIACAO));
+		assertEquals("Entrevista", tipo.get(TipoAvaliacao.ENTREVISTA));
 	}
 
 }
