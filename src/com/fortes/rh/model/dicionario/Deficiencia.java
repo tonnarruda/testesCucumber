66945fbf18c8bf5b10f798a37f59c9b79f6e8a8c
samuelpinheiro <samuelpinheiro@gmail.com>
2011-03-17 14:34:11 -0300

    Adicionar Qualquer Deficiencia no filtro da triagem.

--- src/com/fortes/rh/model/dicionario/Deficiencia.java
+++ src/com/fortes/rh/model/dicionario/Deficiencia.java
@@ -7,13 +7,13 @@ public class Deficiencia extends LinkedHashMap
 {
 	private static final long serialVersionUID = -4297978464057851828L;
 	
-	public static final char SEM_DEFICIENCIA = '0';//usado no importador
+	public static final char SEM_DEFICIENCIA 	= '0';//usado no importador
 	public static final char FISICA 			= '1';
-	public static final char AUDITIVA 		= '2';
+	public static final char AUDITIVA 			= '2';
 	public static final char VISUAL 			= '3';
 	public static final char MENTAL 			= '4';
-	public static final char MULTIPLA 		= '5';
-	public static final char REABILITADO 	= '6';
+	public static final char MULTIPLA 			= '5';
+	public static final char REABILITADO 		= '6';
 
 	public Deficiencia()
 	{
