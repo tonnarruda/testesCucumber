cd360677a6c46e1a885cb1d63569c6f1ce493477
marlussaraiva <marlussaraiva@grupofortes.com.br>
2011-04-20 18:16:27 -0300

    Cucumber

---
+++ features/manage_ces_cadastro_area.feature
@@ -0,0 +1,20 @@
+# language: pt
+
+Funcionalidade: Cadastrar Area Organizacional
+ 
+  @dev 
+  Cenário: Cadastro de Organizacional
+    Dado que eu esteja logado
+    Quando eu acesso o menu "C&S > Cadastros > Áreas Organizacionais"
+    Então eu devo ver o título "Áreas Organizacionais"
+    Quando eu clico no botão "Inserir"
+    Então eu devo ver o título "Inserir Área Organizacional"
+    Quando eu preencho "nome" com "__AREA_DE_TESTE"
+    E eu seleciono "Administração" de "Área Mãe"
+    Quando eu clico no botão "Gravar"
+    Então eu devo ver "Administração > __AREA_DE_TESTE"
+    Quando eu clico em excluir "Administração > __AREA_DE_TESTE"
+    Então eu devo ver "Confirma exclusão?"
+    Quando eu aperto "OK"
+    Então eu devo ver "Área Organizacional excluída com sucesso"  
+    E eu não devo ver "Administração > __AREA_DE_TESTE"  
---
+++ features/support/env.rb
@@ -0,0 +1,23 @@
+require 'selenium-webdriver'
+require 'cucumber/formatter/unicode' # Remove this line if you don't want Cucumber Unicode support
+#require 'cucumber/web/tableish'
+require 'capybara/cucumber'
+require 'capybara/session'
+#require 'cucumber/rails/capybara_javascript_emulation' # Lets you click links with onclick javascript handlers without using @culerity or @javascript
+
+#Selenium::WebDriver.for :firefox
+Capybara.default_driver = :selenium
+Capybara.default_wait_time = 5
+
+# This way we are using Selenium-RC
+Capybara.register_driver :selenium do |app|
+  Capybara::Driver::Selenium.new(app,
+    :browser => :remote,
+    :url => "http://localhost:4444/wd/hub",
+    :desired_capabilities => :firefox)
+end
+# Capybara defaults to XPath selectors rather than Webrat's default of CSS3. In
+# order to ease the transition to Capybara we set the default here. If you'd
+# prefer to use XPath just remove this line and adjust any selectors in your
+# steps to use the XPath syntax.
+Capybara.default_selector = :css
