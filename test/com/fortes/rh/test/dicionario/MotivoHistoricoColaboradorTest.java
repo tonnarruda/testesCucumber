ed5bfaa7db3b242ed85e2ff2e547788d0f640033
samuelpinheiro <samuelpinheiro@gmail.com>
2011-06-01 17:54:33 -0300

    ajuste teste MotivoHistoricoColaborador.

--- test/com/fortes/rh/test/dicionario/MotivoHistoricoColaboradorTest.java
+++ test/com/fortes/rh/test/dicionario/MotivoHistoricoColaboradorTest.java
@@ -10,6 +10,6 @@ public class MotivoHistoricoColaboradorTest extends TestCase
 	{
 		MotivoHistoricoColaborador motivos = new MotivoHistoricoColaborador();
 	
-		assertEquals(7, motivos.size());
+		assertEquals(8, motivos.size());
 	}
 }
