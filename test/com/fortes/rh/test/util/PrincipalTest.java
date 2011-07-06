dce8e12b953f621e02bd69d860fa8cba3024f712
franciscobarroso <franciscobarroso@grupofortes.com.br>
2011-07-06 18:31:51 -0300

    teste principal nao deu

--- test/com/fortes/rh/test/util/PrincipalTest.java
+++ test/com/fortes/rh/test/util/PrincipalTest.java
@@ -14,6 +14,28 @@ public class PrincipalTest extends TestCase
     public void testPrincipal()
     {
     	Principal.main(null); 
+    	//da um checkout
+//    	src/com/fortes/rh/business/geral/applicationContext-business.xml
+//    	src/com/fortes/rh/dao/geral/applicationContext-dao.xml
+//    	src/com/fortes/rh/dao/hibernate/geral/applicationContext-dao-hibernate.xml
+//    	src/com/fortes/rh/util/geradorCodigo/Principal.java
+//    	src/hibernate.cfg.xml
+//    	src/xwork.xml
+//    	test/com/fortes/rh/test/util/PrincipalTest.java
+    	//apagar
+//    	rm src/com/fortes/rh/business/geral/BabauManager.java
+//    	rm src/com/fortes/rh/business/geral/BabauManagerImpl.java
+//    	rm src/com/fortes/rh/dao/geral/BabauDao.java
+//    	rm src/com/fortes/rh/dao/hibernate/geral/BabauDaoHibernate.java
+//    	rm src/com/fortes/rh/model/geral/Babau.java
+//    	rm src/com/fortes/rh/web/action/geral/BabauEditAction.java
+//    	rm src/com/fortes/rh/web/action/geral/xwork-Babau.xml
+//    	rm test/com/fortes/rh/test/business/geral/BabauManagerTest.java
+//    	rm test/com/fortes/rh/test/dao/hibernate/geral/BabauDaoHibernateTest.java
+//    	rm test/com/fortes/rh/test/factory/geral/BabauFactory.java
+//    	rm test/com/fortes/rh/test/web/action/geral/BabauEditActionTest.java
+//    	rm web/WEB-INF/forms/geral/babauEdit.ftl
+//    	rm web/WEB-INF/forms/geral/babauList.ftl
     }
 
 }
