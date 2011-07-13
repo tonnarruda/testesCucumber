815654793e14e9af74d2903c26ac985c38623a12
samuelpinheiro <samuelpinheiro@gmail.com>
2011-07-13 12:28:05 -0300

    Colocar exame palografico em ReS.

--- web/WEB-INF/metadata/update_dev.sql
+++ web/WEB-INF/metadata/update_dev.sql
@@ -1,3 +1,4 @@
 update parametrosdosistema set appversao = '1.1.50.42';--.go
 
 alter table areaorganizacional add column emailsnotificacoes text;--.go
+update papel set papelmae_id = 359 where id=510;--.go
