a2a2cbfbd9a80f498f4984198b43d3def33a217a
franciscobarroso <franciscobarroso@grupofortes.com.br>
2011-08-17 11:12:52 -0300

    percentual de pontos da competencia,

--- src/com/fortes/rh/model/captacao/ConfiguracaoNivelCompetenciaVO.java
+++ src/com/fortes/rh/model/captacao/ConfiguracaoNivelCompetenciaVO.java
@@ -9,6 +9,8 @@ public class ConfiguracaoNivelCompetenciaVO
 {
 
 	private String nome;
+	private int totalPontos;
+	private int totalPontosFaixa;
 	private Collection<MatrizCompetenciaNivelConfiguracao> matrizes;
 
 	public ConfiguracaoNivelCompetenciaVO() {
@@ -18,6 +20,11 @@ public class ConfiguracaoNivelCompetenciaVO
 		super();
 		this.nome = nome;
 		this.matrizes = matrizes;
+	}
+	
+	public void somaTotalPontos(int totalPontos)
+	{
+		this.totalPontos = this.totalPontos + totalPontos;
 	}
 	
 	public String getNome() {
@@ -41,5 +48,17 @@ public class ConfiguracaoNivelCompetenciaVO
 	}
 	public void setMatrizes(Collection<MatrizCompetenciaNivelConfiguracao> matrizes) {
 		this.matrizes = matrizes;
+	}
+	public int getTotalPontos() {
+		return totalPontos;
+	}
+	public void setTotalPontos(int totalPontos) {
+		this.totalPontos = totalPontos;
+	}
+	public int getTotalPontosFaixa() {
+		return totalPontosFaixa;
+	}
+	public void setTotalPontosFaixa(int totalPontosFaixa) {
+		this.totalPontosFaixa = totalPontosFaixa;
 	}
 }
