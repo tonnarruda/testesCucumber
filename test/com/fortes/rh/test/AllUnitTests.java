3c5b321d43f61d722cd9dba361614dd98e57c8db
franciscobarroso <franciscobarroso@grupofortes.com.br>
2011-10-25 08:30:44 -0300

    teste hudson

--- test/com/fortes/rh/test/AllUnitTests.java
+++ test/com/fortes/rh/test/AllUnitTests.java
@@ -703,10 +703,10 @@ public class AllUnitTests extends TestSuite
 //        suite.addTestSuite(IndiceHistoricoEditActionTest.class);
 //        suite.addTestSuite(IndiceHistoricoListActionTest.class);
 
-        suite.addTestSuite(ConfiguracaoLimiteColaboradorDaoHibernateTest.class);
-        suite.addTestSuite(ConfiguracaoLimiteColaboradorManagerTest.class);
-        suite.addTestSuite(QuantidadeLimiteColaboradoresPorCargoDaoHibernateTest.class);
-        suite.addTestSuite(QuantidadeLimiteColaboradoresPorCargoManagerTest.class);
+//        suite.addTestSuite(ConfiguracaoLimiteColaboradorDaoHibernateTest.class);
+//        suite.addTestSuite(ConfiguracaoLimiteColaboradorManagerTest.class);
+//        suite.addTestSuite(QuantidadeLimiteColaboradoresPorCargoDaoHibernateTest.class);
+//        suite.addTestSuite(QuantidadeLimiteColaboradoresPorCargoManagerTest.class);
         suite.addTestSuite(FaturamentoMensalEditActionTest.class);
         suite.addTestSuite(FaturamentoMensalManagerTest.class);
         suite.addTestSuite(FaturamentoMensalDaoHibernateTest.class);
