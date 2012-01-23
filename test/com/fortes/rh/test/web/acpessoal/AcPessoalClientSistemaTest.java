5adb94da4b13c5a89a33b97942e8e24e1284e28e
franciscobarroso <franciscobarroso@grupofortes.com.br>
2012-01-23 11:13:11 -0300

    fechamento da versao

--- test/com/fortes/rh/test/web/acpessoal/AcPessoalClientSistemaTest.java
+++ test/com/fortes/rh/test/web/acpessoal/AcPessoalClientSistemaTest.java
@@ -27,7 +27,7 @@ public class AcPessoalClientSistemaTest extends AcPessoalClientTest
 	{
 		montaMockGrupoAC();
 		
-		assertEquals("1.1.47.1", acPessoalClientsistemaImpl.getVersaoWebServiceAC(empresa));
+		assertEquals("1.1.48.1", acPessoalClientsistemaImpl.getVersaoWebServiceAC(empresa));
 	}
 
 	public void testIdACIntegrado() throws Exception
