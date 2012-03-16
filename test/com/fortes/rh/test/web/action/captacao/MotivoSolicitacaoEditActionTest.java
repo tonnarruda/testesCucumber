12545396565c55ce3eb2ce64bab38b8ae8d3cd7f
samuelpinheiro <samuelpinheiro@gmail.com>
2012-03-16 11:03:10 -0300

    Ajustes testes

--- test/com/fortes/rh/test/web/action/captacao/MotivoSolicitacaoEditActionTest.java
+++ test/com/fortes/rh/test/web/action/captacao/MotivoSolicitacaoEditActionTest.java
@@ -4,7 +4,10 @@ import org.jmock.Mock;
 import org.jmock.MockObjectTestCase;
 
 import com.fortes.rh.business.captacao.MotivoSolicitacaoManager;
+import com.fortes.rh.business.geral.EmpresaManager;
 import com.fortes.rh.model.captacao.MotivoSolicitacao;
+import com.fortes.rh.model.geral.Empresa;
+import com.fortes.rh.test.factory.captacao.EmpresaFactory;
 import com.fortes.rh.test.factory.captacao.MotivoSolicitacaoFactory;
 import com.fortes.rh.web.action.captacao.MotivoSolicitacaoEditAction;
 
@@ -12,13 +15,17 @@ public class MotivoSolicitacaoEditActionTest extends MockObjectTestCase
 {
 	private MotivoSolicitacaoEditAction action;
 	private Mock manager;
-
+	private Mock empresaManager;
+	
     protected void setUp() throws Exception
     {
         super.setUp();
         action = new MotivoSolicitacaoEditAction();
         manager = new Mock(MotivoSolicitacaoManager.class);
         action.setMotivoSolicitacaoManager((MotivoSolicitacaoManager) manager.proxy());
+
+        empresaManager = new Mock(EmpresaManager.class);
+        action.setEmpresaManager((EmpresaManager) empresaManager.proxy());
     }
 
     protected void tearDown() throws Exception
@@ -35,24 +42,32 @@ public class MotivoSolicitacaoEditActionTest extends MockObjectTestCase
 
     public void testPrepareInsert() throws Exception
     {
+    	Empresa empresa = EmpresaFactory.getEmpresa();
+    	action.setEmpresaSistema(empresa);
+    	
     	MotivoSolicitacao motivoSolicitacao = MotivoSolicitacaoFactory.getEntity();
     	motivoSolicitacao.setId(1L);
 
     	action.setMotivoSolicitacao(motivoSolicitacao);
 
     	manager.expects(once()).method("findById").with(eq(motivoSolicitacao.getId())).will(returnValue(motivoSolicitacao));
+    	empresaManager.expects(once()).method("findByIdProjection").with(eq(empresa.getId())).will(returnValue(empresa));
 
     	assertEquals(action.prepareInsert(), "success");
     }
 
     public void testPrepareUpdate() throws Exception
     {
+    	Empresa empresa = EmpresaFactory.getEmpresa();
+    	action.setEmpresaSistema(empresa);
+    	
     	MotivoSolicitacao motivoSolicitacao = MotivoSolicitacaoFactory.getEntity();
     	motivoSolicitacao.setId(1L);
 
     	action.setMotivoSolicitacao(motivoSolicitacao);
 
     	manager.expects(once()).method("findById").with(eq(motivoSolicitacao.getId())).will(returnValue(motivoSolicitacao));
+    	empresaManager.expects(once()).method("findByIdProjection").with(eq(empresa.getId())).will(returnValue(empresa));
 
     	assertEquals(action.prepareUpdate(), "success");
     }
@@ -88,5 +103,4 @@ public class MotivoSolicitacaoEditActionTest extends MockObjectTestCase
     	@SuppressWarnings("unused")
 		Object object = action.getModel();
     }
-
 }
