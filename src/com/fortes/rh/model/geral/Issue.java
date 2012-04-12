7d0468d2d836d281f3549a5b3722e88c1d00bfb3
Barroso <franciscobarrosoflf@gmail.com>
2012-04-12 09:54:26 -0300

    listando as issues

--- src/com/fortes/rh/model/geral/Issue.java
+++ src/com/fortes/rh/model/geral/Issue.java
@@ -10,68 +10,20 @@ public class Issue implements Serializable
 	private String id;
 	private String title;
 	private String body;
-	private String created_at;
-	private String updated_at;
-	private String closed_at;
-	private String state;
-	private String number;
-	private String html_url;
-	private String assignee;
-	private String milestone;
-	private String comments;
-	private String pull_request;
-	private String user;
-	private String url;
-	private String labels;
-	
-	public String getAssignee() {
-		return assignee;
-	}
-	public void setAssignee(String assignee) {
-		this.assignee = assignee;
-	}
-	public String getHtml_url() {
-		return html_url;
-	}
-	public void setHtml_url(String html_url) {
-		this.html_url = html_url;
-	}
-	public String getMilestone() {
-		return milestone;
-	}
-	public void setMilestone(String milestone) {
-		this.milestone = milestone;
-	}
-	public String getComments() {
-		return comments;
-	}
-	public void setComments(String comments) {
-		this.comments = comments;
-	}
-	public String getPull_request() {
-		return pull_request;
-	}
-	public void setPull_request(String pull_request) {
-		this.pull_request = pull_request;
-	}
-	public String getUser() {
-		return user;
-	}
-	public void setUser(String user) {
-		this.user = user;
-	}
-	public String getUrl() {
-		return url;
-	}
-	public void setUrl(String url) {
-		this.url = url;
-	}
-	public String getLabels() {
-		return labels;
-	}
-	public void setLabels(String labels) {
-		this.labels = labels;
-	}
+//	private String created_at;
+//	private String updated_at;
+//	private String closed_at;
+//	private String state;
+//	private String number;
+//	private String html_url;
+//	private String assignee;
+//	private String milestone;
+//	private String comments;
+//	private String pull_request;
+//	private String user;
+//	private String url;
+//	private String labels;
+//	private String patch_url;
 	public String getId() {
 		return id;
 	}
@@ -89,36 +41,6 @@ public class Issue implements Serializable
 	}
 	public void setBody(String body) {
 		this.body = body;
-	}
-	public String getCreated_at() {
-		return created_at;
-	}
-	public void setCreated_at(String created_at) {
-		this.created_at = created_at;
-	}
-	public String getUpdated_at() {
-		return updated_at;
-	}
-	public void setUpdated_at(String updated_at) {
-		this.updated_at = updated_at;
-	}
-	public String getClosed_at() {
-		return closed_at;
-	}
-	public void setClosed_at(String closed_at) {
-		this.closed_at = closed_at;
-	}
-	public String getState() {
-		return state;
-	}
-	public void setState(String state) {
-		this.state = state;
-	}
-	public String getNumber() {
-		return number;
-	}
-	public void setNumber(String number) {
-		this.number = number;
 	}
 	
 }
--- web/WEB-INF/forms/geral/issueList.ftl
+++ web/WEB-INF/forms/geral/issueList.ftl
@@ -17,6 +17,7 @@
 			<a href="prepareUpdate.action?issue.id=${issue.id}"><img border="0" title="Editar" src="<@ww.url value="/imgs/edit.gif"/>"></a>
 			<a href="javascript:;" onclick="javascript:newConfirm('Confirma exclusão?', function(){window.location='delete.action?issue.id=${issue.id}'});"><img border="0" title="<@ww.text name="list.del.hint"/>" src="<@ww.url includeParams="none" value="/imgs/delete.gif"/>"></a>
 		</@display.column>
+		<@display.column property="title" title="Titulo"/>
 	</@display.table>
 	
 	<div class="buttonGroup">
