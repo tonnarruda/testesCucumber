a221492f63db1b776e355c7d89a5cb24b3b53539
samuelpinheiro <samuelpinheiro@gmail.com>
2012-04-17 16:13:27 -0300

    Ajuste Relatorio LTCAT

--- src/com/fortes/rh/web/action/sesmt/PpraEditAction.java
+++ src/com/fortes/rh/web/action/sesmt/PpraEditAction.java
@@ -63,6 +63,7 @@ public class PpraEditAction extends MyActionSupportEdit
 		{
 			parametros = RelatorioUtil.getParametrosRelatorio("PPRA", getEmpresaSistema(), null);
 			parametros.put("EXIBIR_COMPOSICAO_SESMT", exibirComposicaoSesmt);
+			parametros.put("AGRUPADO_POR_AMBIENTE", getEmpresaSistema().getControlaRiscoPor() == 'A');
 			dataSource = ambienteManager.montaRelatorioPpraLtcat(getEmpresaSistema(), estabelecimento.getId(), data, ambienteCheck, gerarPpra, gerarLtcat, exibirComposicaoSesmt);
 		}
 		catch (ColecaoVaziaException e)
