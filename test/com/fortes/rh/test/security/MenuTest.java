cfc3a7389f2d0ea76199c3a023b0ef6d2f6b2dd7
franciscobarroso <franciscobarroso@grupofortes.com.br>
2012-05-16 15:49:19 -0300

    ajuste no icone do contatos

--- test/com/fortes/rh/test/security/MenuTest.java
+++ test/com/fortes/rh/test/security/MenuTest.java
@@ -34,11 +34,11 @@ public class MenuTest extends TestCase
 		menu.append("</li>\n");
 		menu.append("<li><a href='localhost/logout.action' accesskey='a'>S<u>a</u>ir</a></li>\n");
 		menu.append("<li style='float: right; line-height: 0.8em;'><a href='localhost/geral/documentoVersao/list.action' class='versao'> Versão: 1</a></li>\n");
+		menu.append("<li style='float: right; line-height: 0.8em'><a href='contatos.action' title='Contatos'><img src='localhost/imgs/telefone.gif' style='vertical-align: middle;'></a></li>\n");
 		menu.append("<li style='float: right; line-height: 0.8em'><a href='http://twitter.com/#!/entetecnologia' target='_blank' title='Twitter'><img src='localhost/imgs/twitter.png' style='vertical-align: middle;'></a></li>\n");
 		menu.append("<li style='float: right; line-height: 0.8em'><a href='http://sistemaderh.blogspot.com' target='_blank' title='Blog'><img src='localhost/imgs/blog.png' style='vertical-align: middle;'></a></li>\n");
 		menu.append("<li style='float: right; line-height: 0.8em'><a href='videoteca.action' title='Videoteca'><img src='localhost/imgs/video.png' style='vertical-align: middle;'></a></li>\n");
 		menu.append("<li style='float: right; line-height: 0.8em'><a href='http://chatonline.grupofortes.com.br/cliente/MATRIZ/000006/0002' target='_blank' title='Suporte'><img src='localhost/imgs/ChatFortes.gif' style='vertical-align: middle;'></a></li>\n");
-		menu.append("<li style='float: right; line-height: 0.8em'><a href='contatos.action' title='Contatos'><img src='localhost/imgs/telefone.gif' style='vertical-align: middle;'></a></li>\n");
 		menu.append("</ul>\n");
 		menu.append("\n");
 		ParametrosDoSistema parametros = ParametrosDoSistemaFactory.getEntity();
@@ -93,11 +93,11 @@ public class MenuTest extends TestCase
 		menu.append("</li>\n");
 		menu.append("<li><a href='localhost/logout.action' accesskey='a'>S<u>a</u>ir</a></li>\n");
 		menu.append("<li style='float: right; line-height: 0.8em;'><a href='localhost/geral/documentoVersao/list.action' class='versao'> Versão: 1</a></li>\n");
+		menu.append("<li style='float: right; line-height: 0.8em'><a href='contatos.action' title='Contatos'><img src='localhost/imgs/telefone.gif' style='vertical-align: middle;'></a></li>\n");
 		menu.append("<li style='float: right; line-height: 0.8em'><a href='http://twitter.com/#!/entetecnologia' target='_blank' title='Twitter'><img src='localhost/imgs/twitter.png' style='vertical-align: middle;'></a></li>\n");
 		menu.append("<li style='float: right; line-height: 0.8em'><a href='http://sistemaderh.blogspot.com' target='_blank' title='Blog'><img src='localhost/imgs/blog.png' style='vertical-align: middle;'></a></li>\n");
 		menu.append("<li style='float: right; line-height: 0.8em'><a href='videoteca.action' title='Videoteca'><img src='localhost/imgs/video.png' style='vertical-align: middle;'></a></li>\n");
 		menu.append("<li style='float: right; line-height: 0.8em'><a href='http://chatonline.grupofortes.com.br/cliente/MATRIZ/000006/0002' target='_blank' title='Suporte'><img src='localhost/imgs/ChatFortes.gif' style='vertical-align: middle;'></a></li>\n");
-		menu.append("<li style='float: right; line-height: 0.8em'><a href='contatos.action' title='Contatos'><img src='localhost/imgs/telefone.gif' style='vertical-align: middle;'></a></li>\n");
 		menu.append("</ul>\n");
 		menu.append("\n");
 		
