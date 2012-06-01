d2f00d1e4b299298cf9503c51c407193c0aed256
arnaldoescossio <arnaldoescossio@gmail.com>
2012-06-01 17:34:26 -0300

    Melhoria Fechamento de Versao: uniao do create com o create_data com atualizacao pelo update.sql

--- migrate.rb
+++ migrate.rb
@@ -34,30 +34,38 @@ if ARGV.empty? || ARGV[0] == '--db'
 	  end 
 	end	
 elsif ARGV[0] == '--deploy'
-  version = ARGV[1] || 'INSIRA_NUMERO_VERSAO'
-  fortesrh_home = ARGV[2] || '.'
-  
-  last_migrate = File.readlines("#{fortesrh_home}/web/WEB-INF/metadata/update.sql").grep(/insert into migrations values\('(\d{14})'\);--.go/){$1}.last
-  puts "migrate atual no update.sql #{last_migrate}"
-  
-  sql_migrates = ""
-  Dir.glob("#{fortesrh_home}/web/WEB-INF/metadata/migrate/*.sql").sort_by {|f| File.basename f}.each do |file|
-    if (File.basename(file) =~ /^(\d{14})/ and $1 > last_migrate)
-      print "Buscando migrate: #{$1}".ljust(80)
-      sql_migrates << "\n" + File.read(file)
-      sql_migrates << "\ninsert into migrations values('#{$1}');--.go"
-      puts "[ #{green("SUCESSO")} ]"
-    end
-  end
-  
-  if sql_migrates.empty?
-    puts "update.sql ja esta com a migrate mais nova!"
-  else
-    close_version = "\nupdate parametrosdosistema set appversao = '#{version}';--.go"
-    
-    File.open("#{fortesrh_home}/web/WEB-INF/metadata/update.sql",'a'){|f| f.write "\n-- versao #{version}\n" + sql_migrates + close_version}
-    puts "update editado com sucesso."
-  end
+	version = ARGV[1] || 'INSIRA_NUMERO_VERSAO'
+	fortesrh_home = ARGV[2] || '.'
+	
+	last_migrate = File.readlines("#{fortesrh_home}/web/WEB-INF/metadata/update.sql").grep(/insert into migrations values\('(\d{14})'\);--.go/){$1}.last
+	puts "migrate atual no update.sql #{last_migrate}"
+	
+	sql_migrates = ""
+	Dir.glob("#{fortesrh_home}/web/WEB-INF/metadata/migrate/*.sql").sort_by {|f| File.basename f}.each do |file|
+	  if (File.basename(file) =~ /^(\d{14})/ and $1 > last_migrate)
+	    print "Buscando migrate: #{$1}".ljust(80)
+	    sql_migrates << "\n" + File.read(file)
+	    sql_migrates << "\ninsert into migrations values('#{$1}');--.go"
+	    puts "[ #{green("SUCESSO")} ]"
+	  end
+	end
+	
+	if sql_migrates.empty?
+	  puts "update.sql ja esta com a migrate mais nova!"
+	end
+	
+	close_version = "\nupdate parametrosdosistema set appversao = '#{version}';--.go"
+	File.open("#{fortesrh_home}/web/WEB-INF/metadata/update.sql",'a'){|f| f.write "\n-- versao #{version}\n" + sql_migrates + close_version}
+	puts "update editado com sucesso."
+	
+elsif ARGV[0] == '--update-db-vazio'
+  	version = ARGV[1] || 'INSIRA_NUMERO_VERSAO'
+  	fortesrh_home = ARGV[2] || '.'
+  	imprime = false
+	File.readlines("#{fortesrh_home}/web/WEB-INF/metadata/update.sql").each do |line|
+		imprime = true if line.start_with?("-- versao #{version}")
+		exec_sql line if imprime
+	end
 else
 	file_name = ARGV[0]
 	content = ARGV[1] || ''
