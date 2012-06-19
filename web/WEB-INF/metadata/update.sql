c8a3ca9f62a0ea546d19705c0ad8de4061ce4db0
arnaldoescossio <arnaldoescossio@gmail.com>
2012-06-19 11:22:33 -0300

    Versao 1.1.76.71

--- web/WEB-INF/metadata/update.sql
+++ web/WEB-INF/metadata/update.sql
@@ -18976,3 +18976,69 @@ alter sequence papel_sequence restart with 546;--.go
 insert into perfil_papel select perfil_id, 545 from perfil_papel where papeis_id = 8 and perfil_id <> 1;--.go
 insert into migrations values('20120528144454');--.go
 update parametrosdosistema set appversao = '1.1.75.70';--.go
+-- versao 1.1.76.71
+
+update papel set codigo='ROLE_COLAB_LIST' where id=8;--.go
+delete from perfil_papel where papeis_id in (541,542);--.go
+delete from papel where id in (541,542);--.go
+
+INSERT INTO papel (id, codigo, nome, url, ordem, menu, papelmae_id) VALUES (546, 'ROLE_COLAB_LIST_DESLIGAR', 'Desligar', '#', 1, false, 8);--.go
+INSERT INTO papel (id, codigo, nome, url, ordem, menu, papelmae_id) VALUES (547, 'ROLE_COLAB_LIST_ENTREVISTA', 'Entrevista de desligamento', '#', 2, false, 8);--.go
+INSERT INTO papel (id, codigo, nome, url, ordem, menu, papelmae_id) VALUES (548, 'ROLE_COLAB_LIST_EDITAR', 'Editar', '#', 3, false, 8);--.go
+INSERT INTO papel (id, codigo, nome, url, ordem, menu, papelmae_id) VALUES (549, 'ROLE_COLAB_LIST_EXCLUIR', 'Excluir', '#', 4, false, 8);--.go
+update papel set nome='Situação', ordem=5 where id=545;--.go
+INSERT INTO papel (id, codigo, nome, url, ordem, menu, papelmae_id) VALUES (550, 'ROLE_COLAB_LIST_PERFORMANCE', 'Performance', '#', 6, false, 8);--.go
+INSERT INTO papel (id, codigo, nome, url, ordem, menu, papelmae_id) VALUES (551, 'ROLE_COLAB_LIST_NIVELCOMPETENCIA', 'Nível de competência', '#', 7, false, 8);--.go
+INSERT INTO papel (id, codigo, nome, url, ordem, menu, papelmae_id) VALUES (552, 'ROLE_COLAB_LIST_SOLICITACAO', 'Incluir em solicitação', '#', 8, false, 8);--.go
+INSERT INTO papel (id, codigo, nome, url, ordem, menu, papelmae_id) VALUES (553, 'ROLE_COLAB_LIST_DOCUMENTOANEXO', 'Documentos do colaborador', '#', 9, false, 8);--.go
+INSERT INTO papel (id, codigo, nome, url, ordem, menu, papelmae_id) VALUES (554, 'ROLE_COLAB_LIST_VISUALIZARCURRICULO', 'Visualizar currículo', '#', 10, false, 8);--.go
+INSERT INTO papel (id, codigo, nome, url, ordem, menu, papelmae_id) VALUES (555, 'ROLE_CAD_COLABORADOR', 'Outros acessos', '#', 15, false, 8);--.go
+
+alter sequence papel_sequence restart with 556;--.go
+UPDATE parametrosdosistema SET atualizaPapeisIdsAPartirDe=545 WHERE atualizaPapeisIdsAPartirDe is null;--.go
+
+insert into perfil_papel(perfil_id, papeis_id) select perfil_id, 546 from perfil_papel where papeis_id =8;--.go
+insert into perfil_papel(perfil_id, papeis_id) select perfil_id, 547 from perfil_papel where papeis_id =8;--.go
+insert into perfil_papel(perfil_id, papeis_id) select perfil_id, 548 from perfil_papel where papeis_id =8;--.go
+insert into perfil_papel(perfil_id, papeis_id) select perfil_id, 549 from perfil_papel where papeis_id =8;--.go
+insert into perfil_papel(perfil_id, papeis_id) select perfil_id, 550 from perfil_papel where papeis_id =8;--.go
+insert into perfil_papel(perfil_id, papeis_id) select perfil_id, 551 from perfil_papel where papeis_id =8;--.go
+insert into perfil_papel(perfil_id, papeis_id) select perfil_id, 552 from perfil_papel where papeis_id =8;--.go
+insert into perfil_papel(perfil_id, papeis_id) select perfil_id, 553 from perfil_papel where papeis_id =8;--.go
+insert into perfil_papel(perfil_id, papeis_id) select perfil_id, 554 from perfil_papel where papeis_id =8;--.go
+insert into perfil_papel(perfil_id, papeis_id) select perfil_id, 555 from perfil_papel where papeis_id =8;--.go
+insert into migrations values('20120530145356');--.go
+INSERT INTO papel (id, codigo, nome, url, ordem, menu, papelmae_id) VALUES (557, 'ROLE_COLAB_VER_TODOS', 'Visualizar Todos os Colaboradores', '#', 11, false, 8);--.go
+UPDATE parametrosdosistema SET atualizaPapeisIdsAPartirDe=545 WHERE atualizaPapeisIdsAPartirDe is null;--.go
+alter sequence papel_sequence restart with 558;--.go
+insert into migrations values('20120605111418');--.go
+alter table solicitacaoexame add column ordem integer not null default 1;--.go
+insert into migrations values('20120606104515');--.go
+CREATE FUNCTION ajusta_ordem_solicitacao_exame() RETURNS integer AS '
+DECLARE
+    rec RECORD;
+    dat DATE;
+    ord INTEGER;
+BEGIN
+    ord := 0;
+
+    FOR rec IN select * from solicitacaoexame order by data, id
+    LOOP
+		IF dat <> rec.data THEN ord := 0; END IF;
+		update solicitacaoexame set ordem = (ord + 1) where id = rec.id;
+		dat := rec.data;
+		ord := ord + 1;
+    END LOOP;
+
+    RETURN 1;
+END;
+' LANGUAGE plpgsql; --.go
+
+select ajusta_ordem_solicitacao_exame(); --.go
+drop function ajusta_ordem_solicitacao_exame(); --.go
+insert into migrations values('20120606110939');--.go
+alter table solicitacao add column colaboradorsubstituido character varying(100);--.go
+insert into migrations values('20120612104845');--.go
+alter table empresa add column exibircolaboradorsubstituido boolean not null default false;--.go
+insert into migrations values('20120612110829');--.go
+update parametrosdosistema set appversao = '1.1.76.71';--.go
