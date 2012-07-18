4410ff0b048491d06330f7aa9fdb09f4e16fab39
arnaldoescossio <arnaldoescossio@gmail.com>
2012-07-18 16:26:39 -0300

    Ajuste no posicionamento de papeis

---
+++ web/WEB-INF/metadata/migrate/20120718155949_ajuste_posicionamento_papeis.sql
@@ -0,0 +1,3 @@
+update papel set papelmae_id = 365, ordem = 14 where id = 487;--.go
+update papel set papelmae_id = 75, ordem = 7 where id = 474;--.go
+update papel set papelmae_id = 361, ordem = 7 where id = 417;--.go
