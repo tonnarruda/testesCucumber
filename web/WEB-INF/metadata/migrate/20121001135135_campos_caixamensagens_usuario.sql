342a41c3c5e96c112e911e54c4af9a9a6f9758fa
Samuel Pinheiro <samuelpinheiro@entetecnologia.com.br>
2012-10-02 09:39:04 -0300

    Ajuste migrations

--- web/WEB-INF/metadata/migrate/20121001135135_campos_caixamensagens_usuario.sql
+++ web/WEB-INF/metadata/migrate/20121001135135_campos_caixamensagens_usuario.sql
@@ -1,3 +1,4 @@
+insert into migrations (name) values ('20121001135135');--.go
 alter table usuario add column caixasMensagens character varying(255);--.go
 update usuario set caixasMensagens = '{"caixasDireita":["T","C","F","U"],"caixasEsquerda":["P","D","A","S"],"caixasMinimizadas":[]}';--.go
 alter table usuario alter column caixasMensagens set not null; --.go
