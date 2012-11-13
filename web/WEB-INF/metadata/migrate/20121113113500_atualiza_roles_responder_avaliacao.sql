662b86415da75a7ba1ae3d31fbccc1d4340a98ec
Ente Tecnologia <ente@ente.(none)>
2012-11-13 15:09:04 -0300

    Ajustes nas ROLES que permite responder avaliação por outro usuário

---
+++ web/WEB-INF/metadata/migrate/20121113113500_atualiza_roles_responder_avaliacao.sql
@@ -0,0 +1,5 @@
+
+UPDATE papel SET nome = 'Pode Ver e Responder Aval. Desempenho Por Outro Usuário', codigo = 'ROLE_RESPONDER_AVALIACAO_DESEMP_POR_OUTRO_USUARIO', 
+	   papelmae_id = 483, ordem = 1 where id = 484;--.go
+UPDATE papel SET nome = 'Pode Responder Avaliação De Turma Por Outro Usuário', codigo = 'ROLE_RESPONDER_AVALIACAO_TURMA_POR_OUTRO_USUARIO' where id = 487;--.go
+UPDATE papel SET nome = 'Excluir Respostas', papelmae_id = 483, ordem = 2  where id = 560;--.go
