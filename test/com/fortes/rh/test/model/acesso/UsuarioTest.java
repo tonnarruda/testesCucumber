c7d79d5e4d8c4d8515654650728f99aea73577c3
rubensgadelha <rubensgadelha@entetecnologia.com.br>
2012-11-22 14:28:04 -0300

    Correção na configuração padrão de caixas de mensagens para o usuário

--- test/com/fortes/rh/test/model/acesso/UsuarioTest.java
+++ test/com/fortes/rh/test/model/acesso/UsuarioTest.java
@@ -1,8 +1,14 @@
 package com.fortes.rh.test.model.acesso;
 
+import java.util.ArrayList;
+import java.util.List;
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
+
 import junit.framework.TestCase;
 
 import com.fortes.rh.model.acesso.Usuario;
+import com.fortes.rh.model.dicionario.TipoMensagem;
 import com.fortes.rh.test.factory.acesso.UsuarioFactory;
 
 
@@ -26,4 +32,23 @@ public class UsuarioTest extends TestCase
     	assertEquals(false, usuario.confirmarSenha());
     }
 
+    public void testCaixasMensagensPadrao()
+    {
+    	Usuario usuario = UsuarioFactory.getEntity();
+    	TipoMensagem tiposMensagem = new TipoMensagem();
+    	List<Character> tiposUsuario = new ArrayList<Character>();
+    	
+    	Matcher matcher = Pattern.compile("'[A-Z]{1}'").matcher(usuario.getCaixasMensagens());
+    	
+    	while (matcher.find())
+    	{
+    		tiposUsuario.add(matcher.group().charAt(1));
+			assertTrue("Existe caixa de mensagens do tipo " + matcher.group() + " em TipoMensagem", tiposMensagem.containsKey(matcher.group().charAt(1)));
+    	}
+    	
+    	for (Character tipoMensagem : tiposMensagem.keySet())
+    	{
+    		assertTrue("Existe caixa de mensagens do tipo '" + tipoMensagem + "' em Usuario", tiposUsuario.contains(tipoMensagem));
+    	}
+    }
 }
