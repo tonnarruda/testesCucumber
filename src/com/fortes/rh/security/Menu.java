dc4a3f02edaa0a486056318595c3d63101af8a5e
arnaldoescossio <arnaldoescossio@gmail.com>
2013-01-23 16:46:34 -0300

    Link logmein

--- src/com/fortes/rh/security/Menu.java
+++ src/com/fortes/rh/security/Menu.java
@@ -81,6 +81,10 @@ public abstract class Menu
 				"<a href='videoteca.action' title='Videoteca'>" +
 				"<img src='"+ contexto + "/imgs/video.png' style='vertical-align: middle;'></a></li>\n");
 		
+		menu.append("<li style='float: right; line-height: 0.8em'>" +
+				"<a href='http://www.logmein123.com'  target='_blank' title='LogmeIn'>" +
+				"<img src='"+ contexto + "/imgs/video.png' style='vertical-align: middle;'></a></li>\n");
+		
 		if ( parametros != null && !StringUtils.isEmpty(parametros.getCodEmpresaSuporte()) && !StringUtils.isEmpty(parametros.getCodClienteSuporte()))
 			menu.append("<li style='float: right; line-height: 0.8em'>" 
 				+ "<a href='http://chatonline.grupofortes.com.br/cliente/MATRIZ/" + parametros.getCodClienteSuporte() + "/" + parametros.getCodEmpresaSuporte() + "' target='_blank' title='Suporte'>" 
