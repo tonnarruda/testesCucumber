44c37b24b55eba88abb791e54bc49beed6adc104
Arnaldo Escossio <arnaldoescossio@gmail.com>
2014-01-27 15:42:25 -0300

    Versao 1.1.123.144

--- web/WEB-INF/metadata/update.sql
+++ web/WEB-INF/metadata/update.sql
@@ -22303,3 +22303,34 @@ insert into migrations values('20140114135254');--.go
 update parametrosdosistema set acversaowebservicecompativel='1.1.54.1';--.go
 insert into migrations values('20140115105814');--.go
 update parametrosdosistema set appversao = '1.1.123.143';--.go
+-- versao 1.1.123.144
+
+UPDATE papel SET ordem = 9 where id = 22;--.go
+UPDATE papel SET ordem = 10 where id = 56;--.go
+UPDATE papel SET ordem = 11 where id = 556;--.go
+
+INSERT INTO papel (id, codigo, nome, url, ordem, menu, papelmae_id) VALUES (611, 'ROLE_MOV_SOLICITACAO_IMPRIMIR', 'Imprimir', '#', 2, false, 21);--.go 
+INSERT INTO papel (id, codigo, nome, url, ordem, menu, papelmae_id) VALUES (612, 'ROLE_MOV_SOLICITACAO_EDITAR', 'Inserir/Editar', '#', 3, false, 21);--.go 
+INSERT INTO papel (id, codigo, nome, url, ordem, menu, papelmae_id) VALUES (613, 'ROLE_MOV_SOLICITACAO_EXCLUIR', 'Excluir', '#', 4, false, 21);--.go 
+INSERT INTO papel (id, codigo, nome, url, ordem, menu, papelmae_id) VALUES (614, 'ROLE_MOV_SOLICITACAO_ANUNCIAR', 'Anunciar', '#', 5, false, 21);--.go 
+INSERT INTO papel (id, codigo, nome, url, ordem, menu, papelmae_id) VALUES (615, 'ROLE_MOV_SOLICITACAO_ENCERRAR', 'Encerrar', '#', 6, false, 21);--.go 
+INSERT INTO papel (id, codigo, nome, url, ordem, menu, papelmae_id) VALUES (616, 'ROLE_MOV_SOLICITACAO_SUSPENDER', 'Suspender', '#', 7, false, 21);--.go 
+INSERT INTO papel (id, codigo, nome, url, ordem, menu, papelmae_id) VALUES (617, 'ROLE_MOV_SOLICITACAO_CLONAR', 'Clonar', '#', 8, false, 21);--.go 
+
+alter sequence papel_sequence restart with 618;--.go
+
+insert into perfil_papel(perfil_id, papeis_id) select perfil_id, 611 from perfil_papel where papeis_id = 21;--.go
+insert into perfil_papel(perfil_id, papeis_id) select perfil_id, 612 from perfil_papel where papeis_id = 21;--.go
+insert into perfil_papel(perfil_id, papeis_id) select perfil_id, 613 from perfil_papel where papeis_id = 21;--.go
+insert into perfil_papel(perfil_id, papeis_id) select perfil_id, 614 from perfil_papel where papeis_id = 21;--.go
+insert into perfil_papel(perfil_id, papeis_id) select perfil_id, 615 from perfil_papel where papeis_id = 21;--.go
+insert into perfil_papel(perfil_id, papeis_id) select perfil_id, 616 from perfil_papel where papeis_id = 21;--.go
+insert into perfil_papel(perfil_id, papeis_id) select perfil_id, 617 from perfil_papel where papeis_id = 21;--.go
+insert into migrations values('20140120093631');--.go
+update gerenciadorcomunicacao set qtddiaslembrete = '2' where qtddiaslembrete is null and operacao = 8 and meiocomunicacao = 2 and enviarpara = 7;--.go
+update gerenciadorcomunicacao set qtddiaslembrete = '2' where qtddiaslembrete is null and operacao = 9 and meiocomunicacao = 2 and enviarpara = 7;--.go
+update gerenciadorcomunicacao set qtddiaslembrete = '2' where qtddiaslembrete is null and operacao = 9 and meiocomunicacao = 2 and enviarpara = 17;--.go
+update gerenciadorcomunicacao set qtddiaslembrete = '2' where qtddiaslembrete is null and operacao = 9 and meiocomunicacao = 1 and enviarpara = 2;--.go
+update gerenciadorcomunicacao set qtddiaslembrete = '2' where qtddiaslembrete is null and operacao = 25 and meiocomunicacao = 2 and enviarpara = 7;--.go
+insert into migrations values('20140122153122');--.go
+update parametrosdosistema set appversao = '1.1.123.144';--.go
