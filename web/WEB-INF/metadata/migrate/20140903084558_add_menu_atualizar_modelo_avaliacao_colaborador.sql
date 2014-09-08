9e9b6892ca5be9a4159b39847cf7597841ef7b82
Arnaldo Escossio <arnaldoescossio@gmail.com>
2014-09-08 11:06:48 -0300

    Correcao da migrate add_menu_atualizar_modelo_avaliacao_colaborador.sql

--- web/WEB-INF/metadata/migrate/20140903084558_add_menu_atualizar_modelo_avaliacao_colaborador.sql
+++ web/WEB-INF/metadata/migrate/20140903084558_add_menu_atualizar_modelo_avaliacao_colaborador.sql
@@ -1,2 +1,4 @@
 insert into papel (id, codigo, nome, url, ordem, menu, papelmae_id) VALUES (624, 'ROLE_MOV_ATUALIZAR_MODELO_AVALIACAO_COLABORADOR', 'Atualizar Modelos de Avaliação', '/geral/colaborador/prepareAtualizarModeloAvaliacao.action', 4, true, 469);--.go
 insert into perfil_papel(perfil_id, papeis_id) values(1, 624);--.go
+
+alter sequence papel_sequence restart with 625;--.go
