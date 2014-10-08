e266a0bcd780033264dde5a8a8a41cc06747c394
SamuelPinheiro <samuel.flf@gmail.com>
2014-10-08 08:23:11 -0300

    [P20140900133] T&D > Cadastros > Avaliações dos Alunos/Modelos de Avaliação de Curso/Modelos de Avaliação de Aluno: Ajuste na nomeclatura do menu

---
+++ web/WEB-INF/metadata/migrate/20141008081924_update_papel_avaliacoes_da_turma.sql
@@ -0,0 +1,3 @@
+update papel set nome = 'Avaliações dos Alunos' where id = 413;--.go
+update papel set nome = 'Modelos de Avaliação de Curso' where id = 419;--.go
+update papel set nome = 'Modelos de Avaliação de Aluno' where id = 589;--.go
