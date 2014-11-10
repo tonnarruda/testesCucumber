5a6d842bdf892bea5fb0a97ed70a116ef26219ce
Arnaldo Escossio <arnaldoescossio@gmail.com>
2014-11-10 15:10:39 -0300

    [P20141000201] Melhoria de performance em varias consultas.

---
+++ web/WEB-INF/metadata/migrate/20141110145509_create_index_solicitacaoepi_item_solicitacaoepi_id.sql
@@ -0,0 +1,2 @@
+DROP INDEX IF EXISTS index_solicitacaoepi_item_solicitacaoepi_id;--.go
+CREATE INDEX index_solicitacaoepi_item_solicitacaoepi_id ON solicitacaoepi_item (solicitacaoepi_id);--.go
---
+++ web/WEB-INF/metadata/migrate/20141110145546_create_index_historicocolaborador_colaborador_id.sql
@@ -0,0 +1,2 @@
+DROP INDEX IF EXISTS index_historicocolaborador_colaborador_id;--.go
+CREATE INDEX index_historicocolaborador_colaborador_id ON historicocolaborador (colaborador_id);--.go
