ea21408d2ba7f76749fd23d9d72baa14c21c3203
Arnaldo Escossio <arnaldoescossio@gmail.com>
2014-11-26 17:00:08 -0300

    Ajustes

--- src/com/fortes/rh/dao/portalcolaborador/TransacaoPCDao.java
+++
@@ -1,9 +0,0 @@
-package com.fortes.rh.dao.portalcolaborador;
-
-import com.fortes.dao.GenericDao;
-import com.fortes.rh.model.portalcolaborador.TransacaoPC;
-
-public interface TransacaoPCDao extends GenericDao<TransacaoPC>
-{
-	
-}
--- src/com/fortes/rh/model/portalcolaborador/AbstractAdapterPC.java
+++
@@ -1,6 +0,0 @@
-package com.fortes.rh.model.portalcolaborador;
-
-public abstract class AbstractAdapterPC 
-{
-	public abstract String toJson();
-}
