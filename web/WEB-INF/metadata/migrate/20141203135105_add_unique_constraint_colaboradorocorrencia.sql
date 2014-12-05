96b3692c25b71f485d14b757c948bd547f245a89
Rubens Gadelha <rubensgadelha@gmail.com>
2014-12-05 10:45:15 -0300

    Ajuste na criação de chave única para as ocorrências do colaborador, evitando erro ao desfazer a importação feita do Ponto para o AC e do AC para o RH.

--- web/WEB-INF/metadata/migrate/20141203135105_add_unique_constraint_colaboradorocorrencia.sql
+++ web/WEB-INF/metadata/migrate/20141203135105_add_unique_constraint_colaboradorocorrencia.sql
@@ -1,8 +1,2 @@
--- Replica a providencia para todos os itens da duplicacao --.go
-UPDATE colaboradorocorrencia co SET providencia_id = ( SELECT MAX(co2.providencia_id) FROM colaboradorocorrencia co2 WHERE co2.colaborador_id = co.colaborador_id AND co2.ocorrencia_id = co.ocorrencia_id AND co2.dataini = co.dataini );--.go
-
--- Resolve as duplicacoes --.go
-DELETE FROM colaboradorocorrencia WHERE id NOT IN ( SELECT MIN(id) FROM colaboradorocorrencia GROUP BY dataini, colaborador_id, ocorrencia_id );--.go
-
--- Cria a constraint --.go
-ALTER TABLE colaboradorocorrencia ADD CONSTRAINT colaboradorocorrencia_colaborador_ocorrencia_data_uk UNIQUE (colaborador_id, ocorrencia_id, dataini);--.go
+CREATE INDEX colaboradorocorrencia_idx ON colaboradorocorrencia(colaborador_id, ocorrencia_id, dataini);--.go
+DELETE FROM colaboradorocorrencia co WHERE co.id NOT IN (SELECT co2.id FROM colaboradorocorrencia co2 WHERE co2.colaborador_id = co.colaborador_id AND co2.ocorrencia_id = co.ocorrencia_id AND co2.dataini = co.dataini ORDER BY co2.providencia_id NULLS LAST LIMIT 1);--.go
