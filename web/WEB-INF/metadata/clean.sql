ebf0112f24e9c6c4451ec95af96e1dc62fce6dbb
Rafael <rafaellima@grupofortes.com.br>
2015-01-27 13:15:07 -0300

    Retirando linhas que alteravam os emails do clean.sql

--- web/WEB-INF/metadata/clean.sql
+++ web/WEB-INF/metadata/clean.sql
@@ -1,14 +1,5 @@
 update parametrosdosistema set appcontext='/fortesrh', appurl = 'http://127.0.0.1:8080/fortesrh', proximaversao='2020-01-01';
 update grupoac set acurlsoap = 'http://localhost:1024/soap/IAcPessoal',acurlwsdl = 'http://localhost:1024/wsdl/IAcPessoal';
-update empresabds set email='samuelpinheiro@entetecnologia.com.br' where email is not null and email != '';
-update candidato set email='samuelpinheiro@entetecnologia.com.br' where email is not null and email != '';
-update colaborador set email='samuelpinheiro@entetecnologia.com.br' where email is not null and email != '';
-update parametrosdosistema set emailUser='samuelpinheiro@entetecnologia.com.br' where emailUser is not null and emailUser != '';
-update empresa set emailRemetente='samuelpinheiro@entetecnologia.com.br' where emailRemetente is not null and emailRemetente != '';
-update empresa set emailRespSetorPessoal='samuelpinheiro@entetecnologia.com.br' where emailRespSetorPessoal is not null and emailRespSetorPessoal != '';
-update empresa set emailresprh='samuelpinheiro@entetecnologia.com.br' where emailresprh is not null and emailresprh != '';
-update empresa set emailresplimitecontrato='samuelpinheiro@entetecnologia.com.br' where emailresplimitecontrato is not null and emailresplimitecontrato != '';
-update areaorganizacional set emailsnotificacoes = '' where emailsnotificacoes is not null and emailsnotificacoes != '';
 update parametrosdosistema set emailsmtp='mailtrap.io', emailport='465', emailuser='284631bca48fa0a62', emailpass='6979a34aab4468', autenticacao = true, tls = false where emailsmtp is not null and emailsmtp != '';
 update usuario set senha='MTIzNA==';
 update historicocolaborador set salario=5555.00;
