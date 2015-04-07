fd276a8e5d9e43aa683b0bd463454ec053a25c0e
Rafael <rafaellima@grupofortes.com.br>
2015-04-07 11:57:16 -0300

    Adicionando HttpUtil.java

---
+++ src/com/fortes/rh/util/HttpUtil.java
@@ -0,0 +1,26 @@
+package com.fortes.rh.util;
+
+import java.io.IOException;
+
+import org.apache.commons.httpclient.HttpClient;
+import org.apache.commons.httpclient.HttpException;
+import org.apache.commons.httpclient.methods.PostMethod;
+
+public class HttpUtil
+{
+	public static String getHtmlViaPost(String url) throws HttpException, IOException
+	{
+		HttpClient client = new HttpClient();
+	    client.getParams().setParameter("http.useragent", "Test Client");
+
+	    PostMethod method = new PostMethod(url);
+
+	    int returnCode = client.executeMethod(method);
+	    return method.getResponseBodyAsString();
+	}
+	
+	public static String getHtmlViaGet(String url)
+	{
+		return StringUtil.getHTML(url);
+	}
+}
