72623d4a5f557f8dbbce1a3c97d0f9b094e0ec9b
SamuelPinheiro <samuel.flf@gmail.com>
2015-12-30 14:39:09 -0300

    Versao 1.1.156.187

--- web/WEB-INF/metadata/migrate/20151230120940_PE_Nova_aval_desempenho.sql
+++ web/WEB-INF/metadata/migrate/20151230120940_PE_Nova_aval_desempenho.sql
@@ -12,8 +12,6 @@ ALTER TABLE criterioavaliacaocompetencia ADD CONSTRAINT criterioavaliacaocompete
 ALTER TABLE criterioavaliacaocompetencia ADD CONSTRAINT criterioavaliacaocompetencia_atitude_fk FOREIGN KEY (atitude_id) REFERENCES atitude(id);--.go
 CREATE SEQUENCE criterioavaliacaocompetencia_sequence START WITH 1 INCREMENT BY 1 NO MAXVALUE NO MINVALUE CACHE 1;--.go
 
----------------
-
 CREATE TABLE configuracaonivelcompetenciacriterio (
 	id bigint NOT NULL,
 	criterio_id bigint,
@@ -29,13 +27,9 @@ CREATE SEQUENCE configuracaonivelcompetenciacriterio_sequence START WITH 1 INCRE
 
 ALTER TABLE ConfiguracaoNivelCompetencia ADD COLUMN pesocompetencia smallint;--.go
 update configuracaonivelcompetencia set pesocompetencia = 1 where configuracaonivelcompetenciafaixasalarial_id is not null;--.go
-------
 
 ALTER TABLE empresa ADD COLUMN mostrarPerformanceAvalDesempenho boolean default false;--.go
 
----------------
-
---verificar id dos papeis ao criar migrate
 update papel set url = null where id = 516;--.go
 
 INSERT INTO papel (id, codigo, nome, url, ordem, menu, papelmae_id) VALUES (650, 'ROLE_CAD_NIVEL_COMPETENCIA', 'Cadastros', '/captacao/nivelCompetencia/list.action', 1, true, 516);--.go
@@ -45,8 +39,6 @@ INSERT INTO papel (id, codigo, nome, url, ordem, menu, papelmae_id) VALUES (651,
 INSERT INTO perfil_papel (perfil_id, papeis_id) SELECT perfil_id, 651 FROM perfil_papel where papeis_id = 516;--.go
 
 alter sequence papel_sequence restart with 652;--.go
-
----------------
 
 CREATE TABLE nivelCompetenciaHistorico (
 	id bigint NOT NULL,
@@ -71,12 +63,8 @@ ALTER TABLE ConfigHistoricoNivel ADD CONSTRAINT ConfigHistoricoNivel_nivelCompet
 ALTER TABLE ConfigHistoricoNivel ADD CONSTRAINT ConfigHistoricoNivel_nivelCompetenciaHistorico_fk FOREIGN KEY (nivelCompetenciaHistorico_id) REFERENCES nivelCompetenciaHistorico(id);--.go
 CREATE SEQUENCE ConfigHistoricoNivel_sequence START WITH 1 INCREMENT BY 1 NO MAXVALUE NO MINVALUE CACHE 1;--.go
 
-----------------
-
 ALTER TABLE configuracaoNivelCompetenciaFaixaSalarial ADD COLUMN nivelCompetenciaHistorico_id BIGINT;--.go
 ALTER TABLE configuracaoNivelCompetenciaFaixaSalarial ADD CONSTRAINT configNivelCompFaixaSalarial_nivelCompetenciaHistorico_fk FOREIGN KEY (nivelCompetenciaHistorico_id) REFERENCES nivelCompetenciaHistorico(id);--.go
-
-----------------
 
 CREATE OR REPLACE FUNCTION criaConfigHistoricoNivel() RETURNS integer AS $$
 DECLARE 
@@ -88,9 +76,9 @@ BEGIN
 		LOOP 
 			nch_id := nextval('nivelCompetenciaHistorico_sequence');
 
-			INSERT INTO nivelCompetenciaHistorico(id,data,empresa_id) values(nch_id,'2005-01-01',mv.empresa_id);--.go
-			INSERT INTO ConfigHistoricoNivel(id,nivelCompetencia_id,nivelCompetenciaHistorico_id,ordem) SELECT nextval('ConfigHistoricoNivel_sequence'),id, nch_id ,ordem FROM nivelcompetencia WHERE empresa_id = mv.empresa_id;--.go
-			UPDATE configuracaoNivelCompetenciaFaixaSalarial SET nivelcompetenciahistorico_id = nch_id  WHERE faixasalarial_id  IN (SELECT fs.id FROM faixasalarial fs JOIN cargo c ON fs.cargo_id = c.id AND c.empresa_id = mv.empresa_id );--.go
+			INSERT INTO nivelCompetenciaHistorico(id,data,empresa_id) values(nch_id,'2005-01-01',mv.empresa_id);
+			INSERT INTO ConfigHistoricoNivel(id,nivelCompetencia_id,nivelCompetenciaHistorico_id,ordem) SELECT nextval('ConfigHistoricoNivel_sequence'),id, nch_id ,ordem FROM nivelcompetencia WHERE empresa_id = mv.empresa_id;
+			UPDATE configuracaoNivelCompetenciaFaixaSalarial SET nivelcompetenciahistorico_id = nch_id  WHERE faixasalarial_id  IN (SELECT fs.id FROM faixasalarial fs JOIN cargo c ON fs.cargo_id = c.id AND c.empresa_id = mv.empresa_id );
 		END LOOP; 
 		
     RETURN 1; 
@@ -100,8 +88,6 @@ select criaConfigHistoricoNivel();--.go
 drop function criaConfigHistoricoNivel();--.GO
 
 ALTER TABLE nivelCompetencia drop COLUMN ordem;--.go
-
-----------
 
 CREATE TABLE participanteavaliacaodesempenho (
 	id bigint NOT NULL,
@@ -114,9 +100,6 @@ ALTER TABLE participanteavaliacaodesempenho ADD CONSTRAINT participanteavaliacao
 ALTER TABLE participanteavaliacaodesempenho ADD CONSTRAINT participanteavaliacaodesempenho_colaborador_fk FOREIGN KEY (colaborador_id) REFERENCES colaborador(id);--.go
 ALTER TABLE participanteavaliacaodesempenho ADD CONSTRAINT participanteavaliacaodesempenho_avaliacaodesempenho_fk FOREIGN KEY (avaliacaodesempenho_id) REFERENCES avaliacaodesempenho(id);--.go
 CREATE SEQUENCE participanteavaliacaodesempenho_sequence START WITH 1 INCREMENT BY 1 NO MAXVALUE NO MINVALUE CACHE 1;--.go
-
-
-----------------
 
 drop index if exists index_colaboradorquestionario_colaborador;--.go
 create index index_colaboradorquestionario_colaborador on colaboradorquestionario (avaliador_id,avaliacaodesempenho_id,colaborador_id);--.go
@@ -147,11 +130,9 @@ $$ LANGUAGE plpgsql;--.go
 select relaciona_avaliado_avaliador();--.go
 drop function relaciona_avaliado_avaliador();--.go 
 
-delete from colaboradorquestionario where (colaborador_id is null or avaliador_id is null) and avaliacaodesempenho_id is not null;
+delete from colaboradorquestionario where (colaborador_id is null or avaliador_id is null) and avaliacaodesempenho_id is not null;--.go
 
 drop index if exists index_colaboradorquestionario_colaborador;--.go
-
----------------
 
 CREATE FUNCTION insert_participante_avaliador() RETURNS integer AS $$
 DECLARE
@@ -168,8 +149,6 @@ $$ LANGUAGE plpgsql;--.go
 select insert_participante_avaliador();--.go
 drop function insert_participante_avaliador();--.go 
 
----------------
-
 CREATE FUNCTION insert_participante_avaliado() RETURNS integer AS $$
 DECLARE
     mviews RECORD;
@@ -185,13 +164,10 @@ $$ LANGUAGE plpgsql;--.go
 select insert_participante_avaliado();--.go
 drop function insert_participante_avaliado();--.go
 
----------------
 ALTER TABLE participanteavaliacaodesempenho ADD COLUMN produtividade double precision; --.go
 
----------------
 ALTER TABLE colaboradorquestionario ADD COLUMN pesoAvaliador integer default 1; --.go
 
----------
 DELETE from colaboradorquestionario where id in 
 (select cq.id from colaboradorquestionario cq
 	left join avaliacaodesempenho ad on ad.id = cq.avaliacaodesempenho_id 
@@ -200,8 +176,6 @@ DELETE from colaboradorquestionario where id in
 		cq.colaborador_id is null
 		and not(cq.avaliador_id = cq_a.colaborador_id and ad.permiteautoavaliacao = true)
 );--.go
-
-----------
 
 CREATE TABLE configuracaocompetenciaavaliacaodesempenho (
 	id bigint NOT NULL,
@@ -217,10 +191,6 @@ ALTER TABLE configuracaocompetenciaavaliacaodesempenho ADD CONSTRAINT configurac
 ALTER TABLE configuracaocompetenciaavaliacaodesempenho ADD CONSTRAINT configuracaocompetenciaavaliacaodesempenho_configuracaonivelcompetenciafaixasalarial_fk FOREIGN KEY (configuracaonivelcompetenciafaixasalarial_id) REFERENCES configuracaonivelcompetenciafaixasalarial(id);--.go
 ALTER TABLE configuracaocompetenciaavaliacaodesempenho ADD CONSTRAINT configuracaocompetenciaavaliacaodesempenho_avaliacaodesempenho_fk FOREIGN KEY (avaliacaodesempenho_id) REFERENCES avaliacaodesempenho(id);--.go
 CREATE SEQUENCE configuracaocompetenciaavaliacaodesempenho_sequence START WITH 1 INCREMENT BY 1 NO MAXVALUE NO MINVALUE CACHE 1;--.go
-
-------
-
----------Abaixo em 04/12/2015
 
 DELETE FROM configuracaoNivelCompetenciaColaborador WHERE colaborador_id is null or faixasalarial_id is null;--.go
 ALTER TABLE configuracaoNivelCompetenciaColaborador ADD COLUMN ConfiguracaoNivelCompetenciaFaixaSalarial_id bigint;--.go
@@ -258,20 +228,14 @@ UPDATE colaboradorquestionario SET configuracaonivelcompetenciacolaborador_id = 
 
 DELETE FROM configuracaoNivelCompetenciaColaborador WHERE ConfiguracaoNivelCompetenciaFaixaSalarial_id is null;--.go
 
-------15/12/2015
 ALTER TABLE colaboradorQuestionario ADD COLUMN respondidaParcialmente boolean NOT NULL DEFAULT false; --.go
-------
 
 ALTER TABLE avaliacao ADD COLUMN respostasCompactas boolean NOT NULL DEFAULT false; --.go
 
------ 21/12/2015
 INSERT INTO papel (id, codigo, nome, url, ordem, menu, papelmae_id) VALUES (652, 'ROLE_REL_ANALISE_COMPETENCIAS_COLABORADOR', 'Análise de Desempenho das Competências do Colaborador', '/avaliacao/desempenho/prepareAnaliseDesempenhoCompetenciaColaborador.action', 7, true, 486);--.go
 insert into perfil_papel(perfil_id, papeis_id) values(1, 652);--.go
 alter sequence papel_sequence restart with 653;--.go
 
------
-
-------21/12/2015
 ALTER TABLE avaliacaodesempenho ADD COLUMN exiberesultadoautoavaliacao boolean default false;--.go
 ALTER TABLE avaliacaodesempenho ADD COLUMN empresa_id bigint;--.go
 ALTER TABLE avaliacaodesempenho ADD CONSTRAINT avaliacaodesempenho_empresa_fk FOREIGN KEY (empresa_id) REFERENCES empresa(id);--.go
@@ -289,5 +253,3 @@ END;
 $$ LANGUAGE plpgsql;--.go
 select insere_empresa_em_avaliacaodesempenho();--.go
 drop function insere_empresa_em_avaliacaodesempenho();--.go
-
---------
