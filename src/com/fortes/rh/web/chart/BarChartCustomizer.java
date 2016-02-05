a3ca4f89adf34565c2110f8c296427c97cd79875
Arnaldo Escóssio <arnaldoescossio@entetecnologia.com.br>
2016-02-05 13:48:11 -0300

    Ajustes

--- src/com/fortes/rh/web/chart/BarChartCustomizer.java
+++ src/com/fortes/rh/web/chart/BarChartCustomizer.java
@@ -13,40 +13,11 @@ public class BarChartCustomizer  extends JRAbstractChartCustomizer {
 	 
     public void customize(JFreeChart jFreeChart, JRChart jrChart) {
     	CategoryPlot catPlot = jFreeChart.getCategoryPlot();
-        CategoryDataset catDS = catPlot.getDataset();
-        Color seriesColor = null;
         
 //        Object j = getParameterValue("REPORT_DATA_SOURCE");
-        
-//        Collection<TaxaDemissao> taxas = (Collection<TaxaDemissao>) parameter.getValue();
- 
-        for (int i = 0; i < catDS.getColumnCount(); i++) {
-            if (catDS.getColumnKey(i).toString().equalsIgnoreCase("02/2015")) {
-                seriesColor = Color.blue;
-            } else if (catDS.getColumnKey(i).toString().equalsIgnoreCase("Warning")) {
-                seriesColor = Color.green;
-            } else if (catDS.getColumnKey(i).toString().equalsIgnoreCase("Minor")) {
-                seriesColor = Color.red;
-            } else {
-            	seriesColor = Color.gray;
-            }
-            //catPlot.getRendererForDataset(catDS). Paint(seriesColor); // (catDS.getColumnIndex(catDS.getColumnKey(i)), seriesColor);
-            
-//            jrChart.get getXYPlot().setQuadrantPaint(i, seriesColor);
-        }
-        
-//        System.out.println("called customizer");
-//        CategoryPlot plot = jFreeChart.getCategoryPlot();
-//        BarRenderer renderer = (BarRenderer) new BarRenderer();
-//        renderer.setSeriesPaint(0, Color.green);
-//        renderer.setSeriesPaint(1, Color.red);
-//        plot.setRenderer(renderer);
         
         CustomBarRenderer renderer = new CustomBarRenderer();
         jFreeChart.getCategoryPlot().setRenderer(renderer);
         
-//        catPlot.getRendererForDataset(catDS).getSeriesPaint(0);
-//        catPlot.getRendererForDataset(catDS).getSeriesPaint(1);
-//        catPlot.getRendererForDataset(catDS).getSeriesPaint(5);
     }
 }
