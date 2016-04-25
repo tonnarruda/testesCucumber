238ff60b98853b0da63dd5b239f69e1ed6cfe293
Rafael <rafaellima@grupofortes.com.br>
2016-04-25 14:29:05 -0300

    Ajuste na migration 20160425135101_add_role_gestor_visualizar_propria_ocorrencia_providencia.sql

--- web/WEB-INF/metadata/migrate/20160425135101_add_role_gestor_visualizar_propria_ocorrencia_providencia.sql
+++ web/WEB-INF/metadata/migrate/20160425135101_add_role_gestor_visualizar_propria_ocorrencia_providencia.sql
@@ -3,6 +3,8 @@ update papel set ordem = 4 where id = 504;--.go
 
 alter table papel alter column nome TYPE character varying(150);--.go
 
+alter table papel alter column codigo TYPE character varying(80);--.go
+
 insert into papel (id, codigo, nome, url, ordem, menu, papelmae_id) values (672, 'ROLE_MOV_GESTOR_VISUALIZAR_PROPRIA_OCORRENCIA_PROVIDENCIA', 
 'Permitir que o gestor visualize suas próprias ocorrências e providências nas movimentações e relatórios.', '#', 5, false, 373);--.go
 
