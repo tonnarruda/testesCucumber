87931b382015a2292eb08ba23ef1e2084baba10b
SamuelPinheiro <samuel.flf@gmail.com>
2016-11-25 14:25:44 -0300

    [S16110000277] Info. Funcionais > Cadastros > Colaboradores > Performance Profissional: Possibilidade de configurar para que o gestor visualize suas próprias ocorrências.

---
+++ web/WEB-INF/metadata/migrate/20161125134949_update_descricao_papel_672.sql
@@ -0,0 +1,1 @@
+update papel set nome = 'Permitir que o gestor visualize suas próprias ocorrências e providências nas movimentações, relatórios e performance profissional' where id = 672; --.go
--- web/WEB-INF/metadata/migrations.sql
+++
@@ -1,2 +0,0 @@
-
-update papel set nome = 'Permitir que o gestor visualize suas próprias ocorrências e providências nas movimentações, relatórios e performance profissional' where id = 672; --.go
