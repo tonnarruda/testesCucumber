66d825c9416756fd241494e5ee1a5302643cb149
Paula Santos <paulafap@gmail.com>
2017-01-09 08:50:10 -0300

    [Revisão][S16110000355]T&D > Cadastros > Cursos/Treinamentos > Turmas > Avaliações dos Alunos: Permitir manter a nota do colaborador em branco.

--- web/WEB-INF/metadata/clean1.sql
+++ web/WEB-INF/metadata/clean1.sql
@@ -1,5 +1,4 @@
 
-
 update parametrosdosistema set appcontext='/fortesrh', appurl = 'http://127.0.0.1:8080/fortesrh', proximaversao='2020-01-01';
 update grupoac set acurlsoap = 'http://localhost:1024/soap/IAcPessoal',acurlwsdl = 'http://localhost:1024/wsdl/IAcPessoal';
 update parametrosdosistema set emailsmtp='mailtrap.io', emailport='465', emailuser='284638900eb69f13d', emailpass='c464f07aac55db', autenticacao = true, tls = false where emailsmtp is not null and emailsmtp != '';
