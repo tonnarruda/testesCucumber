8ca8dcf263e139bebc7ad766452569a2512a5e41
Tiago Vale <tiagovale@grupofortes.com.br>
2017-10-04 16:31:12 -0300

    S17090000302 Importador: Nova validação para checar a compatibilidade do Importador com o FortesRH.

--- web/WEB-INF/metadata/migrations.sql
+++ web/WEB-INF/metadata/migrations.sql
@@ -6,3 +6,5 @@ INSERT INTO papel (id, codigo, nome, url, ordem, menu, papelmae_id) VALUES (800,
 INSERT INTO perfil_papel(perfil_id, papeis_id) VALUES(1, 800);--.go
 
 ALTER SEQUENCE papel_sequence RESTART WITH 801;--.go
+alter table parametrosdosistema add column versaoImportador character varying(20); --.go
+update parametrosdosistema SET versaoImportador='1.64.0'; --.go
