78bd101dc237bd43f507b7cdde897f5f79017167
Arnaldo Escóssio <arnaldoescossio@entetecnologia.com.br>
2017-10-11 16:36:01 -0300

    Ajuste no sql para redimensionar o campo "nome" do colaborador.

--- web/WEB-INF/metadata/migrate/20170926073828_eSocial_setembro_2017.sql
+++ web/WEB-INF/metadata/migrate/20170926073828_eSocial_setembro_2017.sql
@@ -22,8 +22,40 @@ alter table candidato add column ufHab_id bigint; --.go
 
 ALTER TABLE bairro ALTER COLUMN nome TYPE character varying(60) USING SUBSTR(nome, 1, 60);--.go;
 
-UPDATE pg_attribute SET atttypmod = 70+4 WHERE attrelid = 'colaborador'::regclass AND attname = 'nome'; --.go
+DROP VIEW situacaosolicitacaoepi;--.go
 
+ALTER TABLE colaborador ALTER COLUMN nome TYPE character varying(70);--.go
+
+CREATE OR REPLACE VIEW situacaosolicitacaoepi AS 
+ SELECT sub.solicitacaoepiid, sub.empresaid, sub.estabelecimentoid, sub.estabelecimentonome, sub.colaboradorid, sub.colaboradormatricula, sub.colaboradornome, sub.colaboradordesligado, sub.solicitacaoepidata, sub.cargonome, sub.qtdsolicitado, sub.qtdentregue, 
+        CASE
+            WHEN sub.qtdsolicitado <= sub.qtdentregue THEN 'E'::text
+            WHEN sub.qtdentregue > 0 AND sub.qtdentregue < sub.qtdsolicitado THEN 'P'::text
+            WHEN sub.qtdentregue = 0 THEN 'A'::text
+            ELSE NULL::text
+        END AS solicitacaoepisituacaoentregue, sub.qtddevolvida, 
+        CASE
+            WHEN sub.qtddevolvida <> 0 AND sub.qtddevolvida = sub.qtdentregue THEN 'D'::text
+            WHEN sub.qtddevolvida > 0 AND sub.qtddevolvida < sub.qtdentregue THEN 'DP'::text
+            WHEN sub.qtddevolvida = 0 AND sub.qtdentregue > 0 THEN 'S'::text
+            ELSE NULL::text
+        END AS solicitacaoepisituacaodevolvido
+   FROM ( SELECT se.id AS solicitacaoepiid, se.empresa_id AS empresaid, est.id AS estabelecimentoid, est.nome AS estabelecimentonome, c.id AS colaboradorid, c.matricula AS colaboradormatricula, c.nome AS colaboradornome, c.desligado AS colaboradordesligado, se.data AS solicitacaoepidata, ca.nome AS cargonome, ( SELECT sum(sei2.qtdsolicitado) AS sum
+                   FROM solicitacaoepi_item sei2
+                  WHERE sei2.solicitacaoepi_id = se.id) AS qtdsolicitado, COALESCE(sum(seie.qtdentregue), 0::bigint) AS qtdentregue, COALESCE(sum(seid.qtddevolvida), 0::bigint) AS qtddevolvida
+           FROM solicitacaoepi se
+      LEFT JOIN solicitacaoepi_item sei ON sei.solicitacaoepi_id = se.id
+   LEFT JOIN solicitacaoepiitementrega seie ON seie.solicitacaoepiitem_id = sei.id
+   LEFT JOIN solicitacaoepiitemdevolucao seid ON seid.solicitacaoepiitem_id = sei.id
+   LEFT JOIN colaborador c ON se.colaborador_id = c.id
+   LEFT JOIN historicocolaborador hc ON c.id = hc.colaborador_id
+   LEFT JOIN estabelecimento est ON se.estabelecimento_id = est.id
+   LEFT JOIN cargo ca ON se.cargo_id = ca.id
+  WHERE hc.data = (( SELECT max(hc2.data) AS max
+   FROM historicocolaborador hc2
+  WHERE hc2.colaborador_id = c.id AND hc2.status = 1 AND hc2.data <= 'now'::text::date)) AND hc.status = 1
+  GROUP BY se.id, se.empresa_id, est.id, est.nome, c.matricula, c.id, c.nome, c.desligado, se.data, ca.id, ca.nome) sub;--.go
+  
 ALTER TABLE colaborador ALTER COLUMN pai TYPE character varying(70);--.go
 ALTER TABLE colaborador ALTER COLUMN mae TYPE character varying(70);--.go
 ALTER TABLE colaborador ALTER COLUMN conjuge TYPE character varying(70);--.go
