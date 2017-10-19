60fe43f4d3857873d376a21e96821fb01312f1d1
SamuelPinheiro <samuel.flf@gmail.com>
2017-10-19 08:15:20 -0300

    Ajuste da migration curso empresa

--- web/WEB-INF/metadata/migrate/20171013133238_alter_constraint_curso_empresa_fk.sql
+++ web/WEB-INF/metadata/migrate/20171013133238_alter_constraint_curso_empresa_fk.sql
@@ -1,2 +1,3 @@
 ALTER TABLE ONLY curso DROP CONSTRAINT IF EXISTS curso_empresa;--.go
-ALTER TABLE ONLY curso ADD CONSTRAINT curso_empresa_FK FOREIGN KEY (empresa_id) REFERENCES empresa(id);--.go
+ALTER TABLE ONLY curso DROP CONSTRAINT IF EXISTS curso_empresa_fk;--.go
+ALTER TABLE ONLY curso ADD CONSTRAINT curso_empresa_fk FOREIGN KEY (empresa_id) REFERENCES empresa(id);--.go
