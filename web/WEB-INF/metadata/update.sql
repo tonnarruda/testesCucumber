8c501ce2e143953a2903e93384d7aa72efb926e9
Arnaldo Escóssio <arnaldoescossio@entetecnologia.com.br>
2017-11-21 15:52:46 -0300

    Ajuste no update.sql

--- web/WEB-INF/metadata/update.sql
+++ web/WEB-INF/metadata/update.sql
@@ -28062,8 +28062,28 @@ $$ LANGUAGE plpgsql;--.go
 SELECT insert_nomeFuncao_em_historicofuncao();--.go
 DROP FUNCTION insert_nomeFuncao_em_historicofuncao();--.go
 
-ALTER TABLE funcao DROP CONSTRAINT funcao_cargo_fk;--.go
+ALTER TABLE funcao DROP CONSTRAINT IF EXISTS funcao_cargo_fk;--.go
+
 insert into migrations values('20171009092851');--.go
+
+delete from atitude_curso where cursos_id in (select id from curso where empresa_id not in (select id from empresa) );--.go
+delete from conhecimento_curso where cursos_id in (select id from curso where empresa_id not in (select id from empresa) );--.go
+delete from habilidade_curso where cursos_id in (select id from curso where empresa_id not in (select id from empresa) );--.go
+delete from certificacao_curso where cursos_id in (select id from curso where empresa_id not in (select id from empresa) );--.go
+delete from curso_avaliacaocurso where cursos_id in (select id from curso where empresa_id not in (select id from empresa) );--.go
+delete from curso_empresa where cursos_id in (select id from curso where empresa_id not in (select id from empresa) );--.go
+
+delete from colaboradorresposta where colaboradorquestionario_id in (select id from colaboradorquestionario where turma_id in (select id from turma where curso_id in (select id from curso where empresa_id not in (select id from empresa) )));--.go
+delete from colaboradorquestionario where turma_id in (select id from turma where curso_id in (select id from curso where empresa_id not in (select id from empresa) ));--.go
+delete from colaboradorturma where turma_id in (select id from turma where curso_id in (select id from curso where empresa_id not in (select id from empresa) ));--.go
+delete from diaturma where turma_id in (select id from turma where curso_id in (select id from curso where empresa_id not in (select id from empresa) ));--.go
+delete from turma_avaliacaoturma where turma_id in (select id from turma where curso_id in (select id from curso where empresa_id not in (select id from empresa) ));--.go
+delete from turmatipodespesa where turma_id in (select id from turma where curso_id in (select id from curso where empresa_id not in (select id from empresa) ));--.go
+delete from turma_documentoanexo where turma_id in (select id from turma where curso_id in (select id from curso where empresa_id not in (select id from empresa) ));--.go
+delete from turma where curso_id in (select id from curso where empresa_id not in (select id from empresa) );--.go
+
+delete from curso where empresa_id not in (select id from empresa);--.go
+
 ALTER TABLE ONLY curso DROP CONSTRAINT IF EXISTS curso_empresa;--.go
 ALTER TABLE ONLY curso DROP CONSTRAINT IF EXISTS curso_empresa_fk;--.go
 ALTER TABLE ONLY curso ADD CONSTRAINT curso_empresa_fk FOREIGN KEY (empresa_id) REFERENCES empresa(id);--.go
